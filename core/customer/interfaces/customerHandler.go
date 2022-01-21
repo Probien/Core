aba374c3f65318bfee46f2356f1a1673ab7ac7e0
jairDavid <jairvasmar@gmail.com>
2022-01-20 22:21:32 -0600

    customer endpoints added

--- core/customer/interfaces/customerHandler.go
+++ core/customer/interfaces/customerHandler.go
@@ -14,6 +14,7 @@ func CustomerHandler(v1 *gin.RouterGroup) {
 
 	customerHandlerV1.POST("/", func(c *gin.Context) {
 		customer, err := interactor.Create(c)
+
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
 		}
@@ -21,15 +22,33 @@ func CustomerHandler(v1 *gin.RouterGroup) {
 	})
 
 	customerHandlerV1.GET("/", func(c *gin.Context) {
+		customers, err := interactor.GetAll()
 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"data": "something went wrong"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": customers})
+		}
 	})
 
 	customerHandlerV1.GET("/:id", func(c *gin.Context) {
+		customer, err := interactor.GetById(c)
 
+		if err != nil {
+			c.JSON(http.StatusFound, gin.H{"data": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": customer})
+		}
 	})
 
 	customerHandlerV1.PATCH("/", func(c *gin.Context) {
+		customer, err := interactor.Update(c)
 
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": customer})
+		}
 	})
 
 }
