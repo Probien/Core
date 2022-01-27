7c399874c7f2cf1e526728b9ae62beb13671e7b3
jairDavid <jairvasmar@gmail.com>
2022-01-26 23:17:18 -0600

    hot fix on application core

--- core/pawn_order/application/pawnOrder.go
+++ core/pawn_order/application/pawnOrder.go
@@ -11,21 +11,21 @@ type PawnOrderInteractor struct {
 }
 
 func (PI *PawnOrderInteractor) GetById(c *gin.Context) (domain.PawnOrder, error) {
-	repository := persistance.NewPawnOrderRepositoryImpl(config.GetDBInstance())
+	repository := persistance.NewPawnOrderRepositoryImpl(config.Database)
 	return repository.GetById(c)
 }
 
 func (PI *PawnOrderInteractor) GetAll() ([]domain.PawnOrder, error) {
-	repository := persistance.NewPawnOrderRepositoryImpl(config.GetDBInstance())
+	repository := persistance.NewPawnOrderRepositoryImpl(config.Database)
 	return repository.GetAll()
 }
 
 func (PI *PawnOrderInteractor) Create(c *gin.Context) (domain.PawnOrder, error) {
-	repository := persistance.NewPawnOrderRepositoryImpl(config.GetDBInstance())
+	repository := persistance.NewPawnOrderRepositoryImpl(config.Database)
 	return repository.Create(c)
 }
 
 func (PI *PawnOrderInteractor) Update(c *gin.Context) (domain.PawnOrder, error) {
-	repository := persistance.NewPawnOrderRepositoryImpl(config.GetDBInstance())
+	repository := persistance.NewPawnOrderRepositoryImpl(config.Database)
 	return repository.Update(c)
 }
