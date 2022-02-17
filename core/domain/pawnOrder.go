7e9cd4d793adcd91ecaed8aebd805e1fc8291d56
jairDavid <jairvasmar@gmail.com>
2022-02-16 23:01:47 -0600

    updated pawnOrder struct

--- core/domain/pawnOrder.go
+++ core/domain/pawnOrder.go
@@ -7,9 +7,9 @@ import (
 type PawnOrder struct {
 	ID            uint          `json:"id"`
 	EmployeeID    uint          `json:"employee_id"`
-	StatusID      uint          `json:"status_id"`
 	CustomerID    uint          `json:"customer_id"`
 	Customer      Customer      `json:"customer"`
+	StatusID      uint          `json:"status_id"`
 	TotalMount    float64       `json:"total_mount"`
 	Monthly       bool          `json:"monthly"`
 	Products      []Product     `json:"products"`
