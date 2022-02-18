f69b003cab02b0a31d4e665ab4e53fd1387d5b71
jairDavid <jairvasmar@gmail.com>
2022-02-17 22:30:38 -0600

    endpoints done

--- core/interfaces/branchOfficeHandler.go
+++ core/interfaces/branchOfficeHandler.go
@@ -8,11 +8,12 @@ import (
 )
 
 func BranchOfficeHandler(v1 *gin.RouterGroup) {
+
 	branchOfficeHandlerv1 := *v1.Group("/branch-office")
 	interactor := application.BranchOfficeInteractor{}
 
-	branchOfficeHandlerv1.GET("/", func(c *gin.Context) {
-		employees, err := interactor.GetAll()
+	branchOfficeHandlerv1.POST("/", func(c *gin.Context) {
+		employees, err := interactor.Create(c)
 
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
@@ -20,15 +21,33 @@ func BranchOfficeHandler(v1 *gin.RouterGroup) {
 		c.JSON(http.StatusOK, gin.H{"data": &employees})
 	})
 
-	branchOfficeHandlerv1.GET("/:id", func(c *gin.Context) {
+	branchOfficeHandlerv1.GET("/", func(c *gin.Context) {
+		branchOffices, err := interactor.GetAll()
 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"data": "something went wrong"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": &branchOffices})
+		}
 	})
 
-	branchOfficeHandlerv1.POST("/", func(c *gin.Context) {
+	branchOfficeHandlerv1.GET("/:id", func(c *gin.Context) {
+		branchOffice, err := interactor.GetById(c)
 
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"data": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": &branchOffice})
+		}
 	})
 
 	branchOfficeHandlerv1.PATCH("/", func(c *gin.Context) {
+		branchOffice, err := interactor.Update(c)
 
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"data": &branchOffice})
+		}
 	})
 }
