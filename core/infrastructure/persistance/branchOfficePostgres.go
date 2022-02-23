1f05a9e3aa5f6d8e53d1dc92bae5596d160bfcaa
jairDavid <jairvasmar@gmail.com>
2022-02-22 22:53:23 -0600

    branch office repo impl done

--- core/infrastructure/persistance/branchOfficePostgres.go
+++ core/infrastructure/persistance/branchOfficePostgres.go
@@ -1,6 +1,8 @@
 package persistance
 
 import (
+	"errors"
+
 	"github.com/JairDavid/Probien-Backend/core/domain"
 	"github.com/JairDavid/Probien-Backend/core/domain/repository"
 	"github.com/gin-gonic/gin"
@@ -16,17 +18,54 @@ func NewBranchOfficeRepositoryImp(db *gorm.DB) repository.BranchOfficeRepository
 }
 
 func (r *BranchOfficeRepositoryImp) GetAll() (*[]domain.BranchOffice, error) {
-	return &[]domain.BranchOffice{}, nil
+	var branchOffices []domain.BranchOffice
+
+	if err := r.database.Model(&domain.BranchOffice{}).Find(&branchOffices).Error; err != nil {
+		return nil, errors.New("failed to establish a connection with our database services")
+	}
+	return &branchOffices, nil
 }
 
 func (r *BranchOfficeRepositoryImp) GetById(c *gin.Context) (*domain.BranchOffice, error) {
-	return &domain.BranchOffice{}, nil
+	var branchOffice domain.BranchOffice
+
+	if err := r.database.Model(&domain.BranchOffice{}).Find(&branchOffice, c.Param("id")).Error; err != nil {
+		return nil, errors.New("failed to establish a connection with our database services")
+	}
+
+	if branchOffice.ID == 0 {
+		return nil, errors.New("branch office not found")
+	}
+	return &branchOffice, nil
 }
 
 func (r *BranchOfficeRepositoryImp) Create(c *gin.Context) (*domain.BranchOffice, error) {
-	return &domain.BranchOffice{}, nil
+	var branchOffice domain.BranchOffice
+
+	if err := c.ShouldBindJSON(&branchOffice); err != nil {
+		return nil, errors.New("error binding JSON data, verify fields")
+	}
+
+	if err := r.database.Model(&domain.BranchOffice{}).Create(&branchOffice).Error; err != nil {
+		return nil, errors.New("failed to establish a connection with our database services")
+	}
+	return &branchOffice, nil
 }
 
 func (r *BranchOfficeRepositoryImp) Update(c *gin.Context) (*domain.BranchOffice, error) {
-	return &domain.BranchOffice{}, nil
+	patch, branchOffice := map[string]interface{}{}, domain.BranchOffice{}
+	_, errID := patch["id"]
+
+	if err := c.Bind(&patch); err != nil && !errID {
+		return nil, errors.New("error binding JSON data, verify json format")
+	}
+
+	if err := r.database.Model(&domain.BranchOffice{}).Where("id = ?", patch["id"]).Updates(&patch).Find(&branchOffice).Error; err != nil {
+		return nil, errors.New("failed to establish a connection with our database services")
+	}
+
+	if branchOffice.ID == 0 {
+		return nil, errors.New("branch office not found")
+	}
+	return &branchOffice, nil
 }
