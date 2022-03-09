7990946e50810d2989b32eb3142f558bedfad5a8
jairDavid <jairvasmar@gmail.com>
2022-03-08 23:39:02 -0600

    fixed a null preloading struct into some entities

--- core/domain/pawnOrder.go
+++ core/domain/pawnOrder.go
@@ -8,7 +8,7 @@ type PawnOrder struct {
 	ID            uint          `json:"id"`
 	EmployeeID    uint          `json:"employee_id"`
 	CustomerID    uint          `json:"customer_id"`
-	Customer      Customer      `json:"customer"`
+	Customer      *Customer     `json:"customer"`
 	StatusID      uint          `json:"status_id"`
 	TotalMount    float64       `json:"total_mount"`
 	Monthly       bool          `json:"monthly"`
