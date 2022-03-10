0758e50815a75381f4c3c5510601e0c564b83c66
jairDavid <jairvasmar@gmail.com>
2022-03-09 22:40:33 -0600

    added more fields into branch office entity

--- config/migrations/models/BranchOffice.go
+++ config/migrations/models/BranchOffice.go
@@ -3,9 +3,12 @@ package models
 import "time"
 
 type BranchOffice struct {
-	ID        uint `gorm:"primaryKey"`
-	Employees []Employee
-	Payment   float64
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID         uint `gorm:"primaryKey"`
+	Employees  []Employee
+	BranchName string  `gorm:"type:varchar(30);not null"`
+	Payment    float64 `gorm:"not null"`
+	Address    string  `gorm:"type:varchar(60);not null"`
+	ZipCode    string  `gorm:"type:varchar(5);not null"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
--- core/domain/branchOffice.go
+++ core/domain/branchOffice.go
@@ -3,9 +3,12 @@ package domain
 import "time"
 
 type BranchOffice struct {
-	ID        uint       `json:"branch_office_id"`
-	Employees []Employee `json:"employees"`
-	Payment   float64    `json:"payment"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID         uint       `json:"branch_office_id"`
+	Employees  []Employee `json:"employees"`
+	BranchName string     `json:"branch_name"`
+	Payment    float64    `json:"payment"`
+	Address    string     `json:"address"`
+	ZipCode    string     `json:"zip_code"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
