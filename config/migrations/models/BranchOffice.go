2bf52901a085f6a0927febdf613ffaeaad252b81
jairDavid <jairvasmar@gmail.com>
2022-03-16 22:48:10 -0600

    a field into branch office has been deleted, added http files

--- config/migrations/models/BranchOffice.go
+++ config/migrations/models/BranchOffice.go
@@ -5,10 +5,9 @@ import "time"
 type BranchOffice struct {
 	ID         uint `gorm:"primaryKey"`
 	Employees  []Employee
-	BranchName string  `gorm:"type:varchar(30);not null"`
-	Payment    float64 `gorm:"not null"`
-	Address    string  `gorm:"type:varchar(60);not null"`
-	ZipCode    string  `gorm:"type:varchar(5);not null"`
+	BranchName string `gorm:"type:varchar(30);not null"`
+	Address    string `gorm:"type:varchar(60);not null"`
+	ZipCode    string `gorm:"type:varchar(5);not null"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
