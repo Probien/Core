b8c4932935e8c7edbf5a730cdf095bcf0a6ac5a9
JairDavid <20193tn071@utez.edu.mx>
2022-12-22 22:31:24 -0600

    add sqlmock lib for testing and modify authUtil_test.go file

--- core/infrastructure/auth/authUtils_test.go
+++ core/infrastructure/auth/authUtils_test.go
@@ -4,13 +4,57 @@ import "testing"
 
 func TestAuthoritiesChecker(t *testing.T) {
 
-	expected := true
-	got := checkAuthorities([]string{"---", "", "ROLE_TEST", "ROLE_EMPLOYEE"}, &AuthCustomClaims{Roles: map[string]string{"role_0": "FAKER", "role_1": "FAKER", "role_2": "ROLE_EMPLOYEE", "role_3": "FAKER"}})
+	type errorCaseTest struct {
+		description      string
+		rolesInput       AuthCustomClaims
+		authoritiesInput []string
+		expected         bool
+	}
 
-	if expected != got {
-		t.Errorf("Expected %t, got %t", expected, got)
-	} else {
-		t.Logf("Expected %t, got %t", expected, got)
+	tests := []errorCaseTest{
+		{
+			description: "invalid roles",
+			rolesInput: AuthCustomClaims{
+				Roles: map[string]string{"role_0": "FAKER", "role_1": "FAKER", "role_2": "FAKER", "role_3": "FAKER"},
+			},
+			authoritiesInput: []string{"ROLE_ADMIN", "ROLE_EMPLOYEE"},
+			expected:         false,
+		},
+		{
+			description: "invalid empty roles",
+			rolesInput: AuthCustomClaims{
+				Roles: map[string]string{},
+			},
+			authoritiesInput: []string{"ROLE_ADMIN"},
+			expected:         false,
+		},
+		{
+			description: "invalid role matches",
+			rolesInput: AuthCustomClaims{
+				Roles: map[string]string{"role_0": "ROLE_SUPERVISOR", "role_1": "ROLE_GENERAL"},
+			},
+			authoritiesInput: []string{"ROLE_ADMIN", "ROLE_EMPLOYEE"},
+			expected:         false,
+		},
+		{
+			description: "valid roles",
+			rolesInput: AuthCustomClaims{
+				Roles: map[string]string{"role_0": "ROLE_EMPLOYEE"},
+			},
+			authoritiesInput: []string{"ROLE_ADMIN", "ROLE_EMPLOYEE", "ROLE_GENERAL"},
+			expected:         true,
+		},
+	}
+
+	for _, v := range tests {
+		t.Run(v.description, func(t *testing.T) {
+			got := checkAuthorities(v.authoritiesInput, &v.rolesInput)
+			if v.expected != got {
+				t.Errorf("Expected %t, got %t", v.expected, got)
+			} else {
+				t.Logf("Expected %t, got %t", v.expected, got)
+			}
+		})
 	}
 
 }
--- go.mod
+++ go.mod
@@ -34,6 +34,7 @@ require (
 )
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
